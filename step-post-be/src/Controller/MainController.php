434d18fd953f95ab8f28de41e6bb31fdb73cf38e
CyrilPonsan <cyril.ponsan@gmail.com>
2022-05-07 10:11:55 +0200

    controller ok

--- step-post-be/src/Controller/MainController.php
+++ step-post-be/src/Controller/MainController.php
@@ -2,9 +2,6 @@
 
 namespace App\Controller;
 
-use App\Entity\Courrier;
-use App\Entity\Destinataires;
-use App\Entity\StatutCourrier;
 use App\Repository\CourrierRepository;
 use App\Repository\DestinatairesRepository;
 use App\Repository\ExpediteurRepository;
@@ -30,26 +27,18 @@ class MainController extends AbstractController
         StatutCourrierRepository $statutCourrierRepository,
         Service $service,
     ): Response {
-        if (!$this->getUser()) {
-            return $this->redirectToRoute('app_login');
-        } else {
-            $data = $service->stripTag();
-            $courrier = $courrierRepository->findOneBy(['bordereau' => $data[0]]);
-            if ($courrier !== null) :
-                $statuts = $statutCourrierRepository->findBy(
-                    ['courrier' => $courrier->getId()],
-                    ['date' => 'ASC']
-                );
-                $data = $service->getInfosCourrier($statuts, $courrier);
-                $data[1] = [...$data[1], 'bordereau' => $statuts[0]->getCourrier()->getBordereau()];
-                return $this->json([
-                    'statuts' => $data[0],
-                    'destinataire' => $data[1],
-                ]);
-            else :
-                return $this->json(['statuts' => false]);
-            endif;
-        }
+        $result = $service->searchCourrier(
+            $courrierRepository,
+            $statutCourrierRepository,
+        );
+        if (!$result) :
+            return $this->json(['statuts' => false]);
+        else :
+            return $this->json([
+                'statuts' => $result['statuts'],
+                'destinataire' => $result['destinataire']
+            ]);
+        endif;
     }
 
     #[Route('/api/client/detailsCourrier', name: 'api_detailsCourrier')]
@@ -58,10 +47,10 @@ class MainController extends AbstractController
         CourrierRepository $courrierRepository,
         Service $service,
     ): Response {
-        $data = $service->stripTag();
-        $courrier = $courrierRepository->findOneBy(['id' => $data[0]]);
-        $statuts = $statutCourrierRepository->findBy(['courrier' => $courrier]);
-        $data = $service->getInfosCourrier($statuts, $courrier);
+        $data = $service->detailsCourrier(
+            $statutCourrierRepository,
+            $courrierRepository
+        );
         return $this->json([
             'courrier' => $data[0],
             'destinataire' => $data[1]
@@ -75,42 +64,11 @@ class MainController extends AbstractController
         Service $service,
     ): Response {
         $user = $expediteurRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
-        $destinataires = $destinatairesRepository->findBy(
-            ['expediteur' => $user],
-            ['nom' => 'ASC']
+        $result = $service->adressesFavorites(
+            $destinatairesRepository,
+            $user
         );
-        $nom = "";
-        if (isset($_POST['data'])) :
-            $nom = $service->stripTag()[0];
-            $tmp = array();
-            foreach ($destinataires as $el) :
-                if (str_contains($el->getNom(), $nom)) :
-                    array_push($tmp, $el);
-                endif;
-            endforeach;
-            $destinataires = $tmp;
-        endif;
-        if (count($destinataires) === 0) :
-            return $this->json(['destinataires' => false]);
-        endif;
-        $adresses = array();
-        foreach ($destinataires as $el) :
-            $adresses = [...$adresses, [
-                'id' => $el->getId(),
-                'civilite' => $el->getCivilite(),
-                'prenom' => $el->getPrenom(),
-                'nom' => $el->getNom(),
-                'adresse' => $el->getAdresse(),
-                'complement' => $el->getComplement(),
-                'codePostal' => $el->getCodePostal(),
-                'ville' => $el->getVille(),
-                'telephone' => $el->getTelephone(),
-                'email' => $el->getEmail(),
-            ]];
-        endforeach;
-        return $this->json([
-            'destinataires' => $adresses,
-        ]);
+        return $this->json(['destinataires' => $result]);
     }
 
     #[Route('/api/client/getLogs', name: 'api_getLogs')]
@@ -120,68 +78,14 @@ class MainController extends AbstractController
         StatutCourrierRepository $statutCourrierRepository,
         Service $service,
     ): Response {
-        $tmp = $service->stripTag();
-        $page = $tmp[0];
-        $max = $tmp[1];
-        $nom = $tmp[2];
-        $filtre = $tmp[3];
-        // par defaut les données sont triées par id décroissantes, les variables $sort et $direction (ASC ou DESc) servent à définir un tri différent que celui par défaut
-        if (isset($tmp[4])) :
-            $sort = $tmp[4];
-            $direction = $tmp[5];
-        endif;
         $user = $expediteurRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
-        if ($nom === "") :
-            $datas = $courrierRepository->findBy(
-                ['expediteur' => $user],
-                ['id' => 'DESC']
-            );
-        else :
-            $datas = $courrierRepository->findBy(
-                [
-                    'expediteur' => $user,
-                    'nom' => $nom
-                ],
-                [
-                    'id' => 'DESC'
-                ]
-            );
-        endif;
-        if (count($datas) === 0) :
-            return $this->json(['statuts' => false]);
-        endif;
-        $courriers = array();
-        foreach ($datas as $data) :
-            $statut = $statutCourrierRepository->findBy(
-                ['courrier' => $data->getId()],
-                ['date' => 'DESC']
-            );
-            $tmp = $statut[0]->getStatut()->getEtat();
-            if ($service->isDistributed($tmp, $filtre)) :
-                $courriers = [...$courriers, [
-                    'id' => $statut[0]->getCourrier()->getId(),
-                    'date' => $statut[0]->getDate(),
-                    'nom' => $statut[0]->getCourrier()->getNom(),
-                    'prenom' => $statut[0]->getCourrier()->getPrenom(),
-                    'etat' => $statut[0]->getStatut()->getEtat(),
-                    'bordereau' => $statut[0]->getCourrier()->getBordereau(),
-                ]];
-            endif;
-        endforeach;
-        if (isset($sort)) :
-            $courriers = $service->sortArrayByType($courriers, $sort, $direction);
-        endif;
-        if (count($courriers) < ($max * ($page + 1))) :
-            $length = count($courriers);
-        else :
-            $length = ($max * ($page + 1));
-        endif;
-        $statuts = array();
-        for ($i = ($page * $max); $i < $length; $i++) :
-            array_push($statuts, $courriers[$i]);
-        endfor;
+        $result = $service->getLogs(
+            $courrierRepository,
+            $statutCourrierRepository,
+            $user
+        );
         return $this->json([
-            'statuts' => $statuts,
+            'statuts' => $result,
         ]);
     }
 
@@ -191,14 +95,10 @@ class MainController extends AbstractController
         DestinatairesRepository $destinatairesRepository,
         ManagerRegistry $doctrine
     ): Response {
-        $id = $service->stripTag($_POST['data'])[0];
-        $dest = $destinatairesRepository->findOneBy(['id' => $id]);
-
-        $manager = $doctrine->getManager();
-
-        $manager->remove($dest);
-        $manager->flush();
-
+        $service->deleteAdresse(
+            $destinatairesRepository,
+            $doctrine
+        );
         return $this->json(['result' => true]);
     }
 
@@ -208,73 +108,28 @@ class MainController extends AbstractController
         ManagerRegistry $doctrine,
         Service $service,
     ): Response {
-        $data = $service->stripTag();
-
-        $dest = $destinatairesRepository->findOneBy(['id' => end($data)]);
-
-        $dest->setCivilite($data[0]);
-        $dest->setNom($data[1]);
-        $dest->setPrenom($data[2]);
-        $dest->setAdresse($data[3]);
-        $dest->setComplement($data[4]);
-        $dest->setCodePostal($data[5]);
-        $dest->setVille($data[6]);
-        $dest->setTelephone($data[7]);
-        $dest->setEmail($data[8]);
-
-        $manager = $doctrine->getManager();
-        $manager->persist($dest);
-        $manager->flush();
-
-        return $this->json(['result' => $data]);
+        return $this->json(['result' => $service->editAdresse(
+            $destinatairesRepository,
+            $doctrine
+        )]);
     }
 
     #[Route('/api/client/addAdresse', name: 'api_addAdresse')]
     public function addAdresse(
-        DestinatairesRepository $destinataires,
         Service $service,
         ManagerRegistry $doctrine,
         ExpediteurRepository $expediteurRepository,
     ): Response {
-        $data = $service->stripTag();
-
         $exp = $expediteurRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
-
-        $dest = new Destinataires();
-        $dest->setCivilite($data[0]);
-        $dest->setNom($data[1]);
-        $dest->setPrenom($data[2]);
-        $dest->setAdresse($data[3]);
-        $dest->setComplement($data[4]);
-        $dest->setCodePostal($data[5]);
-        $dest->setVille($data[6]);
-        $dest->setTelephone($data[7]);
-        $dest->setEmail($data[8]);
-        $dest->setExpediteur($exp);
-
-        $manager = $doctrine->getManager();
-        $manager->persist($dest);
-        $manager->flush();
-
+        $service->addAdresse($doctrine, $exp);
         return $this->json(['result' => true]);
     }
 
     #[Route('/api/client/expediteur', name: 'api_expediteur')]
-    public function expediteur(ExpediteurRepository $expediteurRepository): Response
+    public function expediteur(ExpediteurRepository $expediteurRepository, Service $service): Response
     {
         $user = $expediteurRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
-
-        $exp = [
-            'civilite' => $user->getCivilite(),
-            'prenom' => $user->getPrenom(),
-            'nom' => $user->getNom(),
-            'adresse' => $user->getAdresse(),
-            'complement' => $user->getComplement(),
-            'codePostal' => $user->getCodePostal(),
-            'ville' => $user->getVille(),
-        ];
-
-        return $this->json(['exp' => $exp]);
+        return $this->json(['exp' => $service->expediteur($user)]);
     }
 
     #[Route('/api/client/qrcode', name: 'api_qrcode')]
@@ -282,54 +137,21 @@ class MainController extends AbstractController
         ServicesToto $serviceToto,
         Service $service,
         ManagerRegistry $doctrine,
-        CourrierRepository $courrierRepository,
         DestinatairesRepository $destinatairesRepository,
         ExpediteurRepository $expediteurRepository,
         StatutRepository $statutRepository,
-        ): Response
-    {
-        $data = $service->stripTag();
-        $dest = $destinatairesRepository->findOneBy(
-            ['id' => $data[0]]
-        );
+    ): Response {
         $user = $expediteurRepository->findOneBy(['email' => $this->getUser()->getUserIdentifier()]);
-        $courrier = new Courrier();
-        $courrier->setType($data[1]);
-        $courrier->setCivilite($dest->getCivilite());
-        $courrier->setPrenom($dest->getPrenom());
-        $courrier->setNom($dest->getNom());
-        $courrier->setAdresse($dest->getAdresse());
-        $courrier->setComplement($dest->getComplement());
-        $courrier->setCodePostal($dest->getCodePostal());
-        $courrier->setVille($dest->getVille());
-        $courrier->setExpediteur($user);
-        $courrier->setBordereau(0);
-
-        $statutCourrier = new StatutCourrier();
-        $statutCourrier->setDate(new \DateTime());
-        $statut = $statutRepository->findOneBy(['etat' => 'en attente']);
-        $statutCourrier->setStatut($statut);
-        $statutCourrier->setCourrier($courrier);
-
-        $manager = $doctrine->getManager();
-        $manager->persist($courrier);
-        $manager->persist($statutCourrier);
-        $manager->flush();
-
-        $id = $courrier->getId();
-
-        $bordereau = 10000 + $id;
-
-        $courrier->setBordereau($bordereau);
-
-        $manager->persist($courrier);
-        $manager->flush();    
-
-        $qrcode = $serviceToto->qrcode($bordereau);
-        
+        $result = $service->qrcode(
+            $serviceToto,
+            $doctrine,
+            $destinatairesRepository,
+            $statutRepository,
+            $user
+        );
         return $this->json([
-            'qrcode' => $qrcode,
-            'bordereau' => $bordereau,
+            'qrcode' => $result['qrcode'],
+            'bordereau' => $result['bordereau'],
         ]);
     }
 }
--- step-post-be/src/Services/Service.php
+++ step-post-be/src/Services/Service.php
@@ -3,9 +3,15 @@
 namespace App\Services;
 
 use App\Entity\Courrier;
+use App\Entity\Destinataires;
+use App\Entity\Expediteur;
+use App\Entity\StatutCourrier;
 use App\Repository\CourrierRepository;
-use App\Repository\ExpediteurRepository;
+use App\Repository\DestinatairesRepository;
 use App\Repository\StatutCourrierRepository;
+use App\Repository\StatutRepository;
+use Doctrine\Persistence\ManagerRegistry;
+use App\Services\Toto as ServicesToto;
 
 class Service
 {
@@ -38,69 +44,6 @@ class Service
             'ville' => $courrier->getVille(),
         ];
         return [$result, $destinataire];
-    }
-
-    public function getDatas(
-        int $user,
-        ExpediteurRepository $expediteurRepository,
-        CourrierRepository $courrierRepository,
-        StatutCourrierRepository $statutCourrierRepository,
-    ) {
-        $tmp = $this->stripTag();
-        $page = $tmp[0];
-        $max = $tmp[1];
-        $nom = $tmp[2];
-        $filtre = $tmp[3];
-        $user = $expediteurRepository->findOneBy(['id' => $user]);
-        if ($nom === "") :
-            $datas = $courrierRepository->findBy(
-                ['expediteur' => $user],
-                ['id' => 'DESC']
-            );
-        else :
-            $datas = $courrierRepository->findBy(
-                [
-                    'expediteur' => $user,
-                    'nom' => $nom
-                ],
-                [
-                    'id' => 'DESC'
-                ]
-            );
-        endif;
-        if (!$datas) :
-            return false;
-        endif;
-        $courriers = array();
-        foreach ($datas as $data) :
-            $statut = $statutCourrierRepository->findBy(
-                ['courrier' => $data->getId()],
-                ['date' => 'DESC']
-            );
-            $tmp = $statut[0]->getStatut()->getEtat();
-            if ($this->isDistributed($tmp, $filtre)) :
-                array_push($courriers, $statut[0]);
-            endif;
-        endforeach;
-        if (count($courriers) < ($max * ($page + 1))) :
-            $length = count($courriers);
-        else :
-            $length = ($max * ($page + 1));
-        endif;
-        $statuts = array();
-        for ($i = ($page * $max); $i < $length; $i++) :
-            $statuts = [...$statuts, [
-                'id' => $courriers[$i]->getCourrier()->getId(),
-                'type' => $courriers[$i]->getCourrier()->getType(),
-                'date' => $courriers[$i]->getDate(),
-                'nom' => $courriers[$i]->getCourrier()->getNom(),
-                'prenom' => $courriers[$i]->getCourrier()->getPrenom(),
-                'etat' => $courriers[$i]->getStatut()->getEtat(),
-                'bordereau' => $courriers[$i]->getCourrier()->getBordereau(),
-            ]];
-
-        endfor;
-        return $statuts;
     }
 
     public function isDistributed(string $tmp, bool $filtre): bool
@@ -170,4 +113,265 @@ class Service
         return $data;
     }
 
+    public function searchCourrier(
+        CourrierRepository $courrierRepository,
+        StatutCourrierRepository $statutCourrierRepository,
+    ) {
+        $data = $this->stripTag();
+        $courrier = $courrierRepository->findOneBy(['bordereau' => $data[0]]);
+        if ($courrier !== null) :
+            $statuts = $statutCourrierRepository->findBy(
+                ['courrier' => $courrier->getId()],
+                ['date' => 'ASC']
+            );
+            $data = $this->getInfosCourrier($statuts, $courrier);
+            $data[1] = [...$data[1], 'bordereau' => $statuts[0]->getCourrier()->getBordereau()];
+            return ([
+                'statuts' => $data[0],
+                'destinataire' => $data[1],
+            ]);
+        else :
+            return false;
+        endif;
+    }
+
+    public function detailsCourrier(
+        StatutCourrierRepository $statutCourrierRepository,
+        CourrierRepository $courrierRepository,
+    ) {
+        $data = $this->stripTag();
+        $courrier = $courrierRepository->findOneBy(['id' => $data[0]]);
+        $statuts = $statutCourrierRepository->findBy(['courrier' => $courrier]);
+        $data = $this->getInfosCourrier($statuts, $courrier);
+        return $data;
+    }
+
+    public function adressesFavorites(
+        DestinatairesRepository $destinatairesRepository,
+        Expediteur $user,
+    ) {
+        $destinataires = $destinatairesRepository->findBy(
+            ['expediteur' => $user],
+            ['nom' => 'ASC']
+        );
+        $nom = "";
+        if (isset($_POST['data'])) :
+            $nom = $this->stripTag()[0];
+            $tmp = array();
+            foreach ($destinataires as $el) :
+                if (str_contains($el->getNom(), $nom)) :
+                    array_push($tmp, $el);
+                endif;
+            endforeach;
+            $destinataires = $tmp;
+        endif;
+        if (count($destinataires) === 0) :
+            return false;
+        endif;
+        $adresses = array();
+        foreach ($destinataires as $el) :
+            $adresses = [...$adresses, [
+                'id' => $el->getId(),
+                'civilite' => $el->getCivilite(),
+                'prenom' => $el->getPrenom(),
+                'nom' => $el->getNom(),
+                'adresse' => $el->getAdresse(),
+                'complement' => $el->getComplement(),
+                'codePostal' => $el->getCodePostal(),
+                'ville' => $el->getVille(),
+                'telephone' => $el->getTelephone(),
+                'email' => $el->getEmail(),
+            ]];
+        endforeach;
+        return $adresses;
+    }
+
+    public function getLogs(
+        CourrierRepository $courrierRepository,
+        StatutCourrierRepository $statutCourrierRepository,
+        Expediteur $user
+    ) {
+        $tmp = $this->stripTag();
+        $page = $tmp[0];
+        $max = $tmp[1];
+        $nom = $tmp[2];
+        $filtre = $tmp[3];
+        // par defaut les données sont triées par id décroissantes, les variables $sort et $direction (ASC ou DESc) servent à définir un tri différent que celui par défaut
+        if (isset($tmp[4])) :
+            $sort = $tmp[4];
+            $direction = $tmp[5];
+        endif;
+        if ($nom === "") :
+            $datas = $courrierRepository->findBy(
+                ['expediteur' => $user],
+                ['id' => 'DESC']
+            );
+        else :
+            $datas = $courrierRepository->findBy(
+                [
+                    'expediteur' => $user,
+                    'nom' => $nom
+                ],
+                [
+                    'id' => 'DESC'
+                ]
+            );
+        endif;
+        if (count($datas) === 0) :
+            return false;
+        endif;
+        $courriers = array();
+        foreach ($datas as $data) :
+            $statut = $statutCourrierRepository->findBy(
+                ['courrier' => $data->getId()],
+                ['date' => 'DESC']
+            );
+            $tmp = $statut[0]->getStatut()->getEtat();
+            if ($this->isDistributed($tmp, $filtre)) :
+                $courriers = [...$courriers, [
+                    'id' => $statut[0]->getCourrier()->getId(),
+                    'date' => $statut[0]->getDate(),
+                    'nom' => $statut[0]->getCourrier()->getNom(),
+                    'prenom' => $statut[0]->getCourrier()->getPrenom(),
+                    'etat' => $statut[0]->getStatut()->getEtat(),
+                    'bordereau' => $statut[0]->getCourrier()->getBordereau(),
+                ]];
+            endif;
+        endforeach;
+        if (isset($sort)) :
+            $courriers = $this->sortArrayByType($courriers, $sort, $direction);
+        endif;
+        if (count($courriers) < ($max * ($page + 1))) :
+            $length = count($courriers);
+        else :
+            $length = ($max * ($page + 1));
+        endif;
+        $statuts = array();
+        for ($i = ($page * $max); $i < $length; $i++) :
+            array_push($statuts, $courriers[$i]);
+        endfor;
+
+        return $statuts;
+    }
+
+    public function deleteAdresse(
+        DestinatairesRepository $destinatairesRepository,
+        ManagerRegistry $doctrine
+    ) {
+        $id = $this->stripTag($_POST['data'])[0];
+        $dest = $destinatairesRepository->findOneBy(['id' => $id]);
+        $manager = $doctrine->getManager();
+        $manager->remove($dest);
+        $manager->flush();
+    }
+
+    public function editAdresse(
+        DestinatairesRepository $destinatairesRepository,
+        ManagerRegistry $doctrine,
+    ) {
+        $data = $this->stripTag();
+
+        $dest = $destinatairesRepository->findOneBy(['id' => end($data)]);
+
+        $dest->setCivilite($data[0]);
+        $dest->setNom($data[1]);
+        $dest->setPrenom($data[2]);
+        $dest->setAdresse($data[3]);
+        $dest->setComplement($data[4]);
+        $dest->setCodePostal($data[5]);
+        $dest->setVille($data[6]);
+        $dest->setTelephone($data[7]);
+        $dest->setEmail($data[8]);
+
+        $manager = $doctrine->getManager();
+        $manager->persist($dest);
+        $manager->flush();
+
+        return $data;
+    }
+
+    public function addAdresse(
+        ManagerRegistry $doctrine,
+        Expediteur $exp,
+    ) {
+        $data = $this->stripTag();
+        $dest = new Destinataires();
+        $dest->setCivilite($data[0]);
+        $dest->setNom($data[1]);
+        $dest->setPrenom($data[2]);
+        $dest->setAdresse($data[3]);
+        $dest->setComplement($data[4]);
+        $dest->setCodePostal($data[5]);
+        $dest->setVille($data[6]);
+        $dest->setTelephone($data[7]);
+        $dest->setEmail($data[8]);
+        $dest->setExpediteur($exp);
+
+        $manager = $doctrine->getManager();
+        $manager->persist($dest);
+        $manager->flush();
+    }
+
+    public function expediteur(Expediteur $user)
+    {
+        return [
+            'civilite' => $user->getCivilite(),
+            'prenom' => $user->getPrenom(),
+            'nom' => $user->getNom(),
+            'adresse' => $user->getAdresse(),
+            'complement' => $user->getComplement(),
+            'codePostal' => $user->getCodePostal(),
+            'ville' => $user->getVille(),
+        ];
+    }
+
+    public function qrcode(
+        ServicesToto $serviceToto,
+        ManagerRegistry $doctrine,
+        DestinatairesRepository $destinatairesRepository,
+        StatutRepository $statutRepository,
+        Expediteur $user
+    ) {
+        $data = $this->stripTag();
+        $dest = $destinatairesRepository->findOneBy(
+            ['id' => $data[0]]
+        );
+        $courrier = new Courrier();
+        $courrier->setType($data[1]);
+        $courrier->setCivilite($dest->getCivilite());
+        $courrier->setPrenom($dest->getPrenom());
+        $courrier->setNom($dest->getNom());
+        $courrier->setAdresse($dest->getAdresse());
+        $courrier->setComplement($dest->getComplement());
+        $courrier->setCodePostal($dest->getCodePostal());
+        $courrier->setVille($dest->getVille());
+        $courrier->setExpediteur($user);
+        $courrier->setBordereau(0);
+
+        $statutCourrier = new StatutCourrier();
+        $statutCourrier->setDate(new \DateTime());
+        $statut = $statutRepository->findOneBy(['etat' => 'en attente']);
+        $statutCourrier->setStatut($statut);
+        $statutCourrier->setCourrier($courrier);
+
+        $manager = $doctrine->getManager();
+        $manager->persist($courrier);
+        $manager->persist($statutCourrier);
+        $manager->flush();
+
+        $id = $courrier->getId();
+
+        $bordereau = 10000 + $id;
+
+        $courrier->setBordereau($bordereau);
+
+        $manager->persist($courrier);
+        $manager->flush();
+
+        $qrcode = $serviceToto->qrcode($bordereau);
+        return [
+            'qrcode' => $qrcode,
+            'bordereau' => $bordereau
+        ];
+    }
 }
