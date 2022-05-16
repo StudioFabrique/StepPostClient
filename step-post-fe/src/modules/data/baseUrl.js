fa737cff6e987828ab0d096bab228dd71f6b4020
CyrilPonsan <cyril.ponsan@gmail.com>
2022-05-16 09:24:26 +0200

    mediaprint

--- step-post-fe/src/modules/data/baseUrl.js
+++ step-post-fe/src/modules/data/baseUrl.js
@@ -3,15 +3,15 @@ const port = 8000;
 //const url = `https://test-poste.herokuapp.com`;
 
 const url = `http://127.0.0.1`;
-
+ 
 const baseUrl = `${url}:${port}/api/client`;
 
 const logUrl = `${url}:${port}/api/login_check`;
 
 const qrcodeUrl = `${url}:${port}/assets/qrcodes/`;
 
-export { baseUrl, logUrl, qrcodeUrl };
-/* 
+export { baseUrl, logUrl, qrcodeUrl };/* 
+
 const baseUrl = `${url}/api/client`;
 
 const logUrl = `${url}/api/login_check`;
