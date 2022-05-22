16d76232e2dae8b2f84c9dedcb0b2b726bce2139
CyrilPonsan <cyril.ponsan@gmail.com>
2022-05-22 20:39:18 +0200

    type

--- step-post-fe/src/components/Bordereau.jsx
+++ step-post-fe/src/components/Bordereau.jsx
@@ -1,137 +1,173 @@
-import React, { Component } from 'react';
-import { getData, getQrcode, postData } from '../modules/postData';
-import Adresse from './Adresse';
-import '../styles/Bordereau.css';
-import { qrcodeUrl } from '../modules/data';
+import React, { Component } from "react";
+import { getData, getQrcode, postData } from "../modules/postData";
+import Adresse from "./Adresse";
+import "../styles/Bordereau.css";
+import { qrcodeUrl } from "../modules/data";
 import PopupConfirmation from "./PopupConfirmation";
 
 class Bordereau extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { exp: [], dest: [], type: '', qrcode: '', bordereau: '-----', valider: false, isSubmitted: false };
-        this.dest = this.props.adresse;
-        this.msg = "Confirmer l'impression du bordereau svp.";
-    }
+  constructor(props) {
+    super(props);
+    this.state = {
+      exp: [],
+      dest: [],
+      type: "",
+      qrcode: "",
+      bordereau: "-----",
+      valider: false,
+      isSubmitted: false,
+    };
+    this.dest = this.props.adresse;
+    this.msg = "Confirmer l'impression du bordereau svp.";
+  }
 
-    componentDidMount = async () => {
-        const response = await getData('/expediteur');
-        if (!this.dest.telephone) {
-            this.dest.telephone = 'non disponible';
-        }
-        this.setState({ exp: response.exp, dest: this.dest, type: this.props.type });
-
+  componentDidMount = async () => {
+    const response = await getData("/expediteur");
+    if (!this.dest.telephone) {
+      this.dest.telephone = "non disponible";
     }
+    this.setState({
+      exp: response.exp,
+      dest: this.dest,
+      type: this.props.type,
+    });
+  };
 
-    handleRetour = () => {
-        this.props.onRetour();
-    }
+  handleRetour = () => {
+    this.props.onRetour();
+  };
 
-    handleQrCode = async () => {
-        if (!this.state.valider) {
-            let type;
-            if (this.state.type) {
-                type = 1;
-            } else {
-                type = 0;
-            }
-            const response = await postData('/qrcode', [this.state.dest.id, type]);
-            this.setState({
-                qrcode: `${response.qrcode}`,
-                bordereau: response.bordereau,
-                valider: true,
-                isSubmitted: true
-            });
-        } else {
-            alert("Votre bordereau est déjà en cours d'impression.");
-        }
+  handleQrCode = async () => {
+    if (!this.state.valider) {
+      let type;
+      if (this.state.type === "lettre") {
+        type = 1;
+      } else {
+        type = 0;
+      }
+      const response = await postData("/qrcode", [this.state.dest.id, type]);
+      this.setState({
+        qrcode: `${response.qrcode}`,
+        bordereau: response.bordereau,
+        valider: true,
+        isSubmitted: true,
+      });
+    } else {
+      alert("Votre bordereau est déjà en cours d'impression.");
     }
+  };
 
-    handleConfirm = () => {
-        window.print();
-        this.setState({isSubmitted: false});
-    }
+  handleConfirm = () => {
+    window.print();
+    this.setState({ isSubmitted: false });
+  };
 
-    handleCancel = () => {
-        this.setState({isSubmitted: false});
-    }
+  handleCancel = () => {
+    this.setState({ isSubmitted: false });
+  };
 
-    render() {
-        return (
-            <main  className='main-bordereau'>
-                <section className='section-bordereau-exp'>
-                    <article className='article-exp-left'>
-                        <div>
-                            <img src="img/logo.png" alt="logo step post" />
-                            <div>
-                                <input type="checkbox" name="type" checked={this.state.type === 'lettre'} readOnly />
-                                <label htmlFor="lettre">Lettre</label>
-                                <input type="checkbox" name="type" checked={this.state.type === 'colis'} readOnly />
-                                <label htmlFor="colis">Colis</label>
-                            </div>
-                            <p>Date et cachet STEP POST</p>
-                        </div>
-                        <div>
-                            <p>STEP POST Opérateur Postal</p>
-                            <p>Autorisation n°12-0098</p>
-                            <p>Technopolis Hélioparc</p>
-                            <p>2 av. du Président Pierre Angot 64000 PAU</p>
-                            <p>RCS de Pau 434 805 644</p>
-                        </div>
-                    </article>
-                    <article className="article-exp-right">
-                        <div>
-                            <h2>SIGNEO</h2>
-                            <p>Le service recommandé de Step Post</p>
-                        </div>
-                        <div>
-                            <h3>Expéditeur</h3>
-                            <span>
-                                <Adresse adresse={this.state.exp} isDest={false} />
-                            </span>
-                        </div>
-                    </article>
-                </section>
-                <section className='section-bordereau-dest'>
-                    <article className='article-dest-left'>
-                        <div>
-                            {
-                                this.state.qrcode ?
-                                    <img src={this.state.qrcode} alt='qrcode' /> :
-                                    <h5>Le QR code et le numéro de bordereau seront générés lors de l'impression.</h5>
-                            }
-                        </div>
-                        <div>
-                            <p>Bordereau n° {this.state.bordereau}</p>
-                        </div>
-                    </article>
-                    <article className='article-dest-right'>
-                        <div>
-                            <h3>Destinataire</h3>
-                            <span>
-                                <Adresse adresse={this.state.dest} instructions={this.props.instructions} isDest={true} />
-                            </span>
-                        </div>
-                        <div>
-                            <p>Signature :</p>
-                            <span>
-                                <p>Date :</p>
-                                <p> .. /  ..  /  .. </p>
-                            </span>
-                        </div>
-                    </article>
-                </section>
-                <section className='section-bordereau-buttons' >
-                    <div>
-                        <button className='button' onClick={this.handleRetour}>Retour</button>
-                        <button className='button-valider' onClick={this.handleQrCode}>Imprimer</button>
-                    </div>
-                </section>
-                {
-                    this.state.isSubmitted && <PopupConfirmation msg={this.msg} onCancelClick={this.handleCancel} onConfirmClick={this.handleConfirm} />
-                }
-            </main>
-        );
-    }
+  render() {
+    return (
+      <main className="main-bordereau">
+        <section className="section-bordereau-exp">
+          <article className="article-exp-left">
+            <div>
+              <img src="img/logo.png" alt="logo step post" />
+              <div>
+                <input
+                  type="checkbox"
+                  name="type"
+                  checked={this.state.type === "lettre"}
+                  readOnly
+                />
+                <label htmlFor="lettre">Lettre</label>
+                <input
+                  type="checkbox"
+                  name="type"
+                  checked={this.state.type === "colis"}
+                  readOnly
+                />
+                <label htmlFor="colis">Colis</label>
+              </div>
+              <p>Date et cachet STEP POST</p>
+            </div>
+            <div>
+              <p>STEP POST Opérateur Postal</p>
+              <p>Autorisation n°12-0098</p>
+              <p>Technopolis Hélioparc</p>
+              <p>2 av. du Président Pierre Angot 64000 PAU</p>
+              <p>RCS de Pau 434 805 644</p>
+            </div>
+          </article>
+          <article className="article-exp-right">
+            <div>
+              <h2>SIGNEO</h2>
+              <p>Le service recommandé de Step Post</p>
+            </div>
+            <div>
+              <h3>Expéditeur</h3>
+              <span>
+                <Adresse adresse={this.state.exp} isDest={false} />
+              </span>
+            </div>
+          </article>
+        </section>
+        <section className="section-bordereau-dest">
+          <article className="article-dest-left">
+            <div>
+              {this.state.qrcode ? (
+                <img src={this.state.qrcode} alt="qrcode" />
+              ) : (
+                <h5>
+                  Le QR code et le numéro de bordereau seront générés lors de
+                  l'impression.
+                </h5>
+              )}
+            </div>
+            <div>
+              <p>Bordereau n° {this.state.bordereau}</p>
+            </div>
+          </article>
+          <article className="article-dest-right">
+            <div>
+              <h3>Destinataire</h3>
+              <span>
+                <Adresse
+                  adresse={this.state.dest}
+                  instructions={this.props.instructions}
+                  isDest={true}
+                />
+              </span>
+            </div>
+            <div>
+              <p>Signature :</p>
+              <span>
+                <p>Date :</p>
+                <p> .. / .. / .. </p>
+              </span>
+            </div>
+          </article>
+        </section>
+        <section className="section-bordereau-buttons">
+          <div>
+            <button className="button" onClick={this.handleRetour}>
+              Retour
+            </button>
+            <button className="button-valider" onClick={this.handleQrCode}>
+              Imprimer
+            </button>
+          </div>
+        </section>
+        {this.state.isSubmitted && (
+          <PopupConfirmation
+            msg={this.msg}
+            onCancelClick={this.handleCancel}
+            onConfirmClick={this.handleConfirm}
+          />
+        )}
+      </main>
+    );
+  }
 }
 
 export default Bordereau;
