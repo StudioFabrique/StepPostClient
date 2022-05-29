06b4a0278c7d22e8e50fbf73bf284b9184fadab2
CyrilPonsan <cyril.ponsan@gmail.com>
2022-05-29 22:55:13 +0200

    renommage

--- backend/src/Entity/Expediteur.php
+++ backend/src/Entity/Expediteur.php
@@ -4,7 +4,6 @@ namespace App\Entity;
 
 use App\Repository\ExpediteurRepository;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ExpediteurRepository::class)]
