b1a40c8c445ff5fb8f8640c50386216b003e19c7
CyrilPonsan <cyril.ponsan@gmail.com>
2022-06-04 17:22:38 +0200

    fixtures

--- backend/migrations/Version20220604152013.php
+++ backend/migrations/Version20220604152013.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220516070815 extends AbstractMigration
+final class Version20220604152013 extends AbstractMigration
 {
     public function getDescription(): string
     {
